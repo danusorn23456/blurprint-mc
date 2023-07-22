c4500d82dd399edbe1772407456879606592e794
danusorn23456 <danusorn23456@gmail.com>
2023-07-23 02:51:11 +0700

    ajust z position relative with width

--- src/sections/nbt-renderer.tsx
+++ src/sections/nbt-renderer.tsx
@@ -1,6 +1,6 @@
 import { CameraControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { ComponentType } from "react";
+import { ComponentType, useLayoutEffect, useRef } from "react";
 import { Block, BlockTextureLoader } from "~/components";
 import { NBT, TagType } from "~/types";
 
@@ -25,13 +25,7 @@ export interface NbtRendererProps {
 const NbtCanvas = (Component: ComponentType) => {
   return (
     <Canvas>
-      <scene>
-        <camera>
-          <ambientLight intensity={0.4} />
-          <Component />
-        </camera>
-        <CameraControls makeDefault />
-      </scene>
+      <Component />
     </Canvas>
   );
 };
@@ -60,28 +54,35 @@ function simplify(data: NBT): simplifyNBT {
 
 const NbtRenderer = ({ nbt }: NbtRendererProps) =>
   NbtCanvas(() => {
+    const cameraControlRef = useRef<CameraControls | null>(null);
     const { blocks, palette } = simplify(nbt);
 
     const maxWidth = Math.max(...blocks.map((b) => b.pos[0])) || 0;
     const maxHeight = Math.max(...blocks.map((b) => b.pos[1])) || 0;
 
+    useLayoutEffect(() => {
+      cameraControlRef.current?.setPosition(0, 0, -maxWidth);
+    });
+
     return (
-      <BlockTextureLoader names={palette.map((p) => p.Name)}>
-        {blocks && (
-          <group
-            position={[-maxWidth / 2, -maxHeight / 2, -maxWidth]}
-            rotation={[0, 0, 0]}
-          >
-            {blocks.map((block, index) => (
-              <Block
-                key={index}
-                name={palette[block.state].Name}
-                position={block.pos}
-              />
-            ))}
-          </group>
-        )}
-      </BlockTextureLoader>
+      <>
+        <CameraControls ref={cameraControlRef} />
+        <scene position={[0, 0, 0]}>
+          <ambientLight intensity={0.4} />
+          <camera position={[-maxWidth / 2, -maxHeight / 2, 0]}>
+            <BlockTextureLoader names={palette.map((p) => p.Name)}>
+              {blocks &&
+                blocks.map((block, index) => (
+                  <Block
+                    key={index}
+                    name={palette[block.state].Name}
+                    position={block.pos}
+                  />
+                ))}
+            </BlockTextureLoader>
+          </camera>
+        </scene>
+      </>
     );
   });
 
